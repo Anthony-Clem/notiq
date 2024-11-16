e859e07a2fe473e87f05d5f0a5518779762afadc
Anthony Clement <antclem6@gmail.com>
2024-11-16 11:42:16 -0500

    mobile auth wrapper

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from "react";
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-neutral-100">
-      <Card className="sm:max-w-[540px] max-sm:h-screen min-w-screen w-full sm:p-12 p-6 flex flex-col items-center">
+      <Card className="sm:max-w-[540px] max-sm:h-screen min-w-screen w-full sm:p-12 p-6 flex flex-col items-center max-sm:justify-center">
         <Logo />
         {children}
       </Card>
