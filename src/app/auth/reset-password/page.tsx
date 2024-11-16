4ef9fd0cb8d9a410d5e4098d111878f8f77ca475
Anthony Clement <antclem6@gmail.com>
2024-11-16 11:33:43 -0500

    suspense

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,14 +1,24 @@
 import AuthWrapper from "@/components/auth/auth-wrapper";
 import ResetPasswordForm from "@/components/auth/reset-password-form";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 const ResetPassword = () => {
   return (
-    <AuthWrapper
-      cardTitle="Reset Your Password"
-      cardDescription="Choose a new password to secure your account."
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          <Loader2 className="animate-spin text-primary" />
+        </div>
+      }
     >
-      <ResetPasswordForm />
-    </AuthWrapper>
+      <AuthWrapper
+        cardTitle="Reset Your Password"
+        cardDescription="Choose a new password to secure your account."
+      >
+        <ResetPasswordForm />
+      </AuthWrapper>
+    </Suspense>
   );
 };
 
